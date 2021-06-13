c3e32694ccb3d0525aeb3fac198bc4e1885a910a
ftshijt <728307998@qq.com>
2021-06-13 04:25:26 -0400

    set config

--- egs/kiritan/svs1/run.sh
+++ egs/kiritan/svs1/run.sh
@@ -24,24 +24,6 @@ test_sets="dev eval1"
 train_config=conf/train.yaml
 inference_config=conf/decode.yaml
 
-# Input example: こ、こんにちは
-
-# 1. Phoneme + Pause
-# (e.g. k o pau k o N n i ch i w a)
-# g2p=pyopenjtalk
-
-# 2. Kana + Symbol
-# (e.g. コ 、 コ ン ニ チ ワ)
-# g2p=pyopenjtalk_kana
-
-# 3. Phoneme + Accent
-# (e.g. k 1 0 o 1 0 k 5 -4 o 5 -4 N 5 -3 n 5 -2 i 5 -2 ch 5 -1 i 5 -1 w 5 0 a 5 0)
-# g2p=pyopenjtalk_accent
-
-# 4. Phoneme + Accent + Pause
-# (e.g. k 1 0 o 1 0 pau k 5 -4 o 5 -4 N 5 -3 n 5 -2 i 5 -2 ch 5 -1 i 5 -1 w 5 0 a 5 0)
-# g2p=pyopenjtalk_accent_with_pause
-
 ./svs.sh \
     --lang jp \
     --feats_type raw \
@@ -49,7 +31,6 @@ inference_config=conf/decode.yaml
     --n_fft "${n_fft}" \
     --n_shift "${n_shift}" \
     --token_type phn \
-    --cleaner jaconv \
     --train_config "${train_config}" \
     --inference_config "${inference_config}" \
     --train_set "${train_set}" \
