a9a299edd37b0e8a07b9b5054e22bef482da1d30
ftshijt <728307998@qq.com>
2021-09-10 01:24:03 -0400

    apply black

--- egs/TEMPLATE/svs1/pyscripts/audio/format_wav_midi_scp.py
+++ egs/TEMPLATE/svs1/pyscripts/audio/format_wav_midi_scp.py
@@ -9,11 +9,17 @@ import numpy as np
 
 from muskit.fileio.sound_scp import SoundScpWriter, SoundScpReader
 from muskit.fileio.midi_scp import MIDIScpWriter, MIDIScpReader
-from muskit.fileio.read_text import read_2column_text, load_num_sequence_text, load_label_sequence
+from muskit.fileio.read_text import (
+    read_2column_text,
+    load_num_sequence_text,
+    load_label_sequence,
+)
+
 
 def pack_zero(file_id, number, length=4):
     number = str(number)
     return file_id + "_" + "0" * (length - len(number)) + number
+
 
 def get_parser():
     parser = argparse.ArgumentParser(
@@ -22,29 +28,39 @@ def get_parser():
     )
     parser.add_argument("scp", type=str, help="scp folder")
     parser.add_argument("outdir", type=str, help="segmented data folder path")
-    parser.add_argument("fs", type=np.int16, default=None, help="If the sampling rate specified, " "Change the sampling rate.")
+    parser.add_argument(
+        "fs",
+        type=np.int16,
+        default=None,
+        help="If the sampling rate specified, " "Change the sampling rate.",
+    )
     parser.add_argument("nj", type=int, default=40, help="number of workers.")
     return parser
+
 
 def makedir(data_url):
     if os.path.exists(data_url):
         shutil.rmtree(data_url)
     Path(data_url).mkdir(parents=True, exist_ok=True)
 
+
 def get_subsequence(start_time, end_time, seq, rate):
     if rate is not None:
-        start = int(start_time*rate)
-        end = int(end_time*rate)
+        start = int(start_time * rate)
+        end = int(end_time * rate)
         if start < 0:
             start = 0
         if end > len(seq):
             end = len(seq)
     else:
-        start = np.searchsorted([item[0] for item in seq], start_time, 'left')
-        end = np.searchsorted([item[1] for item in seq], end_time, 'left')
-    return seq[start: end]
+        start = np.searchsorted([item[0] for item in seq], start_time, "left")
+        end = np.searchsorted([item[1] for item in seq], end_time, "left")
+    return seq[start:end]
 
-def segmentation(key, segment_begin, segment_end, args_fs, args_outdir, rate, wave, note_seq):
+
+def segmentation(
+    key, segment_begin, segment_end, args_fs, args_outdir, rate, wave, note_seq
+):
 
     out_wavscp = Path(args_outdir) / "wav.scp"
     out_midiscp = Path(args_outdir) / "midi.scp"
@@ -53,9 +69,7 @@ def segmentation(key, segment_begin, segment_end, args_fs, args_outdir, rate, wa
     out_mididir = Path(args_outdir) / "midi"
 
     if args_fs is not None and args.fs != rate:
-        wave = resampy.resample(
-            wave.astype(np.float64), rate, args_fs, axis=0
-        )
+        wave = resampy.resample(wave.astype(np.float64), rate, args_fs, axis=0)
         wave = wave.astype(np.int16)
         rate = args_fs
 
@@ -67,45 +81,43 @@ def segmentation(key, segment_begin, segment_end, args_fs, args_outdir, rate, wa
         out_wavscp,
         format="wav",
     )
-    midi_writer = MIDIScpWriter(
-        out_mididir,
-        out_midiscp,
-        format="midi",
-        rate=args_fs
-    )
+    midi_writer = MIDIScpWriter(out_mididir, out_midiscp, format="midi", rate=args_fs)
 
     wav_writer[key] = int(rate), sub_wav
     midi_writer[key] = sub_note
+
 
 if __name__ == "__main__":
     args = get_parser().parse_args(sys.argv[1:])
 
     label_reader = {}
-    with open(args.scp+"/wav.scp") as f:
+    with open(args.scp + "/wav.scp") as f:
         for line in f:
             if len(line) == 0:
                 continue
-            recording_id, path = line.replace('\n', '').split(' ')
+            recording_id, path = line.replace("\n", "").split(" ")
             lab_path = path.replace("wav/", "mono_label/").replace(".wav", ".lab")
             assert os.path.exists(lab_path)
 
             labels = []
             with open(lab_path) as lab_f:
                 for lin in lab_f:
-                    label = lin.replace('\n', '').split(' ')
+                    label = lin.replace("\n", "").split(" ")
                     if len(label) != 3:
                         continue
                     labels.append([float(label[0]), float(label[1]), label[2]])
             label_reader[recording_id] = labels
 
     segments = {}
-    with open(args.scp+"/segments") as f:
+    with open(args.scp + "/segments") as f:
         for line in f:
             if len(line) == 0:
                 continue
-            key, segment_begin, segment_end = line.replace('\n', '').split(' ')
-            utt_id, seg_id = key.split('_')
-            seq = get_subsequence(float(segment_begin), float(segment_end), label_reader[utt_id], None)
+            key, segment_begin, segment_end = line.replace("\n", "").split(" ")
+            utt_id, seg_id = key.split("_")
+            seq = get_subsequence(
+                float(segment_begin), float(segment_end), label_reader[utt_id], None
+            )
             if len(seq) == 0:
                 continue
             segments[key] = seq
@@ -113,18 +125,17 @@ if __name__ == "__main__":
     out_wavscp = Path(args.outdir) / "wav.scp"
     out_midiscp = Path(args.outdir) / "midi.scp"
 
-
     out_wavdir = Path(args.outdir) / "wav"
     out_mididir = Path(args.outdir) / "midi"
 
     makedir(out_wavdir)
     makedir(out_mididir)
 
-    wav_reader = SoundScpReader(args.scp+"/wav.scp")
+    wav_reader = SoundScpReader(args.scp + "/wav.scp")
     if args.fs is not None:
-        midi_reader = MIDIScpReader(args.scp+"/midi.scp", rate=np.int16(args.fs))
+        midi_reader = MIDIScpReader(args.scp + "/midi.scp", rate=np.int16(args.fs))
     else:
-        midi_reader = MIDIScpReader(args.scp+"/midi.scp")
+        midi_reader = MIDIScpReader(args.scp + "/midi.scp")
 
     if args.fs is None:
         args.fs = midi_reader.rate
@@ -134,18 +145,14 @@ if __name__ == "__main__":
         out_wavscp,
         format="wav",
     )
-    midi_writer = MIDIScpWriter(
-        out_mididir,
-        out_midiscp,
-        format="midi",
-        rate=args.fs
-    )
+    midi_writer = MIDIScpWriter(out_mididir, out_midiscp, format="midi", rate=args.fs)
 
     for key, val in segments.items():
         segment_begin = val[0][0]
         segment_end = val[-1][1]
-        uttid, seg_id = key.split('_')
+        uttid, seg_id = key.split("_")
         rate, wave = wav_reader[uttid]
         note_seq = midi_reader[uttid]
-        segmentation(key, segment_begin, segment_end, args.fs, args.outdir, rate, wave, note_seq)
-
+        segmentation(
+            key, segment_begin, segment_end, args.fs, args.outdir, rate, wave, note_seq
+        )
--- egs/kiritan/svs1/local/dataset_split.py
+++ egs/kiritan/svs1/local/dataset_split.py
@@ -6,6 +6,7 @@ import random
 import shutil
 from shutil import copyfile
 
+
 def get_parser():
     parser = argparse.ArgumentParser(
         description="Prepare segments from HTS-style alignment files",
@@ -17,6 +18,7 @@ def get_parser():
     parser.add_argument("eval1", type=float, help="absolute size of dev dataset")
     return parser
 
+
 def find_files_by_extensions(root, exts=[]):
     def _has_ext(name):
         if not exts:
@@ -26,10 +28,12 @@ def find_files_by_extensions(root, exts=[]):
             if name.endswith(ext):
                 return True
         return False
+
     for path, _, files in os.walk(root):
         for name in files:
             if _has_ext(name):
                 yield os.path.join(path, name)
+
 
 def makedir(data_url):
     if os.path.exists(data_url):
@@ -37,47 +41,49 @@ def makedir(data_url):
 
     os.makedirs(data_url)
 
+
 if __name__ == "__main__":
     args = get_parser().parse_args(sys.argv[1:])
-    if args.db_root[-1] != '/':
-        args.db_root = args.db_root + '/'
+    if args.db_root[-1] != "/":
+        args.db_root = args.db_root + "/"
 
-    if args.save_dir[-1] != '/':
-        args.save_dir = args.save_dir + '/'
+    if args.save_dir[-1] != "/":
+        args.save_dir = args.save_dir + "/"
 
     source_root_url = args.db_root
 
     data_root_url = args.save_dir
-    data_train_url = data_root_url+"train_raw"
-    data_vaild_url = data_root_url+"dev_raw"
-    data_test_url = data_root_url+"eval1_raw"
+    data_train_url = data_root_url + "train_raw"
+    data_vaild_url = data_root_url + "dev_raw"
+    data_test_url = data_root_url + "eval1_raw"
     makedir(data_root_url)
     makedir(data_train_url)
     makedir(data_vaild_url)
     makedir(data_test_url)
 
     dataset = os.listdir(source_root_url)
-    dev = int(len(dataset)*args.dev)
-    eval = int(len(dataset)*args.eval1)
+    dev = int(len(dataset) * args.dev)
+    eval = int(len(dataset) * args.eval1)
     train = len(dataset) - dev - eval
-    print(f'trainset={train}, vaild_set={dev}, eval1_set={eval}.')
+    print(f"trainset={train}, vaild_set={dev}, eval1_set={eval}.")
     if train <= 0:
         print("Error, train set is empty.")
         exit()
     # print(dataset)
     random.shuffle(dataset)
-    #copyfile(source_file, destination_file)
+    # copyfile(source_file, destination_file)
     train_set = dataset[:train]
-    validation_set = dataset[train: train + dev]
-    test_set = dataset[train + dev:]
+    validation_set = dataset[train : train + dev]
+    test_set = dataset[train + dev :]
+
 
 def transition(dataset, des_url):
     # src_len = len(source_root_url)
-    path = [des_url+item for item in ['/wav/', '/mono_label/', '/midi_label/'] ]
+    path = [des_url + item for item in ["/wav/", "/mono_label/", "/midi_label/"]]
     for p in path:
         makedir(p)
     for item in dataset:
-        wav_path = source_root_url+item
+        wav_path = source_root_url + item
         lab_path = wav_path.replace("wav/", "mono_label/").replace(".wav", ".lab")
         midi_path = wav_path.replace("wav/", "midi_label/").replace(".wav", ".mid")
 
@@ -88,6 +94,7 @@ def transition(dataset, des_url):
         copyfile(wav_path, des_wav)
         copyfile(lab_path, des_lab)
         copyfile(midi_path, des_midi)
+
 
 transition(train_set, data_train_url)
 transition(validation_set, data_vaild_url)
