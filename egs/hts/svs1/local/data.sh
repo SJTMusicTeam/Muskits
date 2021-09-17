bbcbb804f64e064a92321b7aa6b2dacab5b14260
DrWelles <934356561@qq.com>
2021-09-17 08:09:13 +0000

    HTS update

--- egs/hts/svs1/local/data.sh
+++ egs/hts/svs1/local/data.sh
@@ -4,76 +4,76 @@ set -e
 set -u
 set -o pipefail
 
+. ./path.sh || exit 1;
+. ./cmd.sh || exit 1;
+. ./db.sh || exit 1;
+
 log() {
     local fname=${BASH_SOURCE[1]##*/}
     echo -e "$(date '+%Y-%m-%dT%H:%M:%S') (${fname}:${BASH_LINENO[0]}:${FUNCNAME[1]}) $*"
 }
-SECONDS=0
 
-stage=1
-stop_stage=3
+SECONDS=0
+stage=0
+stop_stage=100
 
 log "$0 $*"
-. utils/parse_options.sh
 
-if [ $# -ne 0 ]; then
-    log "Error: No positional arguments are required."
-    exit 2
+. utils/parse_options.sh || exit 1;
+
+if [ -z "${HTS}" ]; then
+    log "Fill the value of 'HTS' of db.sh"
+    exit 1
 fi
 
-HTS=/data2/qt
-db_root=${HTS}
+mkdir -p ${HTS}
 
 train_set=tr_no_dev
 train_dev=dev
 recog_set=eval1
 
-#cd /data2/qt/Muskits/egs/kiritan
-cd ..
-pwd
-
-#if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
-#    log "stage -1: Data Download"
-#    local/download.sh "${db_root}"
-#fi
-
-#[ -e "${wav_scp}" ] && rm "${wav_scp}"
-#[ -e "${midi_scp}" ] && rm "${midi_scp}"
-#[ -e "${text_scp}" ] && rm "${text_scp}"
-
 if [ ${stage} -le -1 ] && [ ${stop_stage} -ge -1 ]; then
-    log "stage -1: Dataset split "
-    local/dataset_split.py ${db_root}/HTS-demo_NIT-SONG070-F001/data/raw `pwd`/data 0.1 0.1
+    log "stage -1: Data Download"
+    # The HTS data should be downloaded from http://hts.sp.nitech.ac.jp/archives/2.3/HTS-demo_NIT-SONG070-F001.tar.bz2 HTS-demo_NIT-SONG070-F001.tar.bz2
+    # with Facebook authentication
+
 fi
+
+if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
+    log "stage 0: Data prepare"
+    python local/prepare_data.py ${HTS}/HTS-demo_NIT-SONG070-F001/data/raw ${HTS}/HTS-demo_NIT-SONG070-F001/data/labels/mono data/local/raw \
+    --label_type r \
+    --wav_extention raw \
+    --sil pau sil
+fi
+
+if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
+    log "stage 1: Dataset split "
+    python local/dataset_split.py data/local/raw/ data/local 0.1 0.1
+fi
+
 
 for dataset in train dev eval1; do
   echo "process for subset: ${dataset}"
   # dataset=test
-  if [ ${stage} -le 0 ] && [ ${stop_stage} -ge 0 ]; then
-      log "stage 0: local/data_pre.sh"
-      # scp files generation
-      local/data_pre.sh data/${dataset}_raw data/${dataset} 48000
-  fi
-
-  if [ ${stage} -le 1 ] && [ ${stop_stage} -ge 1 ]; then
-      log "stage 1: local/prep_segments.py"
-      local/prep_segments.py data/${dataset} 13500 60 30 > data/${dataset}/segments
-  fi
-
-#  if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-#      log "stage 2: local/format_scp.py"
-#      local/format_scp.py data/${dataset} data/${dataset}_seg 22050 40
-#  fi
-
   if [ ${stage} -le 2 ] && [ ${stop_stage} -ge 2 ]; then
-      log "stage 2: local/format_other_scp.py"
-      local/format_other_scp.py data/${dataset} data/${dataset}_seg
+      log "stage 2: Generate data directory"
+      # scp files generation
+      local/data_pre.sh data/local/${dataset}_raw data/${dataset} 48000
   fi
 
   if [ ${stage} -le 3 ] && [ ${stop_stage} -ge 3 ]; then
-    log "stage 3: local/format_wav_midi_scp.py"
-    local/format_wav_midi_scp.py data/${dataset} data/${dataset}_seg 22050 40
+      log "stage 3: Prepare segments"
+      src_data=data/${dataset}
+      local/prep_segments.py --silence pau --silence sil ${src_data} 10000 # in ms
+      mv ${src_data}/segments.tmp ${src_data}/segments
+      mv ${src_data}/label.tmp ${src_data}/label
+      mv ${src_data}/text.tmp ${src_data}/text
+      cat ${src_data}/segments | awk '{printf("%s kiritan\n", $1);}' > ${src_data}/utt2spk
+      utils/utt2spk_to_spk2utt.pl < ${src_data}/utt2spk > ${src_data}/spk2utt
+      utils/fix_data_dir.sh --utt_extra_files label ${src_data}
   fi
+
 
 done
 
