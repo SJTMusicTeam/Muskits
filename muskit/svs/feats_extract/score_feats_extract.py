a45fd21ef7d4ef22fb21aa85e02bc28d2010742d
DrWelles <934356561@qq.com>
2021-09-26 09:30:09 +0000

    fix mode issue

--- muskit/svs/feats_extract/score_feats_extract.py
+++ muskit/svs/feats_extract/score_feats_extract.py
@@ -96,8 +96,8 @@ class FrameLabelAggregate(AbsFeatsExtract):
         )
 
         # Step3: aggregate label
-        output = torch.gt(output.sum(dim=2, keepdim=False), self.win_length // 2)
-        output = output.float()
+        # (bs, nframe, self.win_length, label_dim) => (bs, nframe)
+        output, _ = output.sum(dim=2, keepdim=False).mode(dim=-1,keepdim=False)
 
         # Step4: process lengths
         if input_lengths is not None:
