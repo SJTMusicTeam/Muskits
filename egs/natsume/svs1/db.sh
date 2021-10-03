7e891fb4ce1caecac53b986a05bc526dde5a2043
Nan-Huo <55927481+Nan-Huo@users.noreply.github.com>
2021-10-03 20:50:47 +0800

    Delete egs/natsume/svs1 directory

--- egs/natsume/svs1/db.sh
+++
@@ -1,9 +0,0 @@
-# Set the path of your corpus
-# "downloads" means the corpus can be downloaded by the recipe automatically
-
-Natsume=../download/
-
-# For only JHU environment
-if [[ "$(hostname -d)" == clsp.jhu.edu ]]; then
-    Natsume=/export/c06/jiatong/svs/SVS_system/egs/public_dataset/natsume/downloads/
-fi
