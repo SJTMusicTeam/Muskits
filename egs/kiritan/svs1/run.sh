2b62dbdfbd91cba660ef8b9c2c67b016b6e9fe4f
ftshijt <ftshijt@gmail.com>
2022-02-18 16:05:25 +0000

    minro local update

--- egs/kiritan/svs1/run.sh
+++ egs/kiritan/svs1/run.sh
@@ -5,9 +5,6 @@ set -e
 set -u
 set -o pipefail
 
-. ./path.sh || exit 1
-. ./cmd.sh || exit 1
-
 # spectrogram-related arguments
 fs=24000
 fmin=80
@@ -44,8 +41,8 @@ cleaner=none
 
 ./svs.sh \
     --lang jp \
-    --stage 0 \
-    --local_data_opts "--stage 0 $(pwd)" 
+    --stage 7 \
+    --local_data_opts "--stage 0 $(pwd)" \
     --feats_type raw \
     --pitch_extract None \
     --fs "${fs}" \
