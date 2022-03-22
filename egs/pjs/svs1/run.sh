38cd3c20bc2547eb395565ad2a12fee6650826b3
Yuning Wu <38801033+A-Quarter-Mile@users.noreply.github.com>
2022-03-22 11:07:02 +0800

    Update egs/pjs/svs1/run.sh
    Co-authored-by: Jiatong <728307998@qq.com>

--- egs/pjs/svs1/run.sh
+++ egs/pjs/svs1/run.sh
@@ -15,8 +15,6 @@ n_shift=300
 win_length=1200
 
 score_feats_extract=frame_score_feats   # frame_score_feats | syllable_score_feats
-expdir=exp/rnn
-# inference_model=196epoch.pth
 
 opts=
 if [ "${fs}" -eq 48000 ]; then
